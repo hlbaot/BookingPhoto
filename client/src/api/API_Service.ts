e132e2d45e77436240f096eafe84b60653d5bc28
Hoang Lam Bao Toan <160599423+hlbaot@users.noreply.github.com>
2026-01-07 22:50:21 +0700

    Clean up API_Service by removing mock URL
    Removed commented-out mock API URL from API_Service.

--- client/src/api/API_Service.ts
+++ client/src/api/API_Service.ts
@@ -4,7 +4,6 @@ import { Package } from "../interfaces/package";
 export const API_Service = async (): Promise<Package[]> => {
   const res = await axios.get<Package[]>(
     "http://localhost:8080/packages"
-    // "https://68d20c23cc7017eec5425e30.mockapi.io/service"
   );
   return res.data;
 };
