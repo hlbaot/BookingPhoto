3fb755994a38d6502596ca12af21f77a04de495e
Hoang Lam Bao Toan <160599423+hlbaot@users.noreply.github.com>
2026-01-08 00:14:28 +0700

    Add additional image domains to next.config.ts

--- client/next.config.ts
+++ client/next.config.ts
@@ -4,9 +4,13 @@ const nextConfig: NextConfig = {
   compiler: {
     styledComponents: true,
   },
-
   images: {
-    domains: ["i.pinimg.com"],
+    domains: [
+      "i.pinimg.com",
+      "res.cloudinary.com",
+      "firebasestorage.googleapis.com",
+      "lh3.googleusercontent.com",
+    ],
   },
 };
 
