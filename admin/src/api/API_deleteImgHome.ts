c85d4cb682af41edb987c88e14d9f72311598665
Hoang Lam Bao Toan <160599423+hlbaot@users.noreply.github.com>
2026-01-09 09:18:57 +0700

    Update image deletion API endpoint to production URL

--- admin/src/api/API_deleteImgHome.ts
+++ admin/src/api/API_deleteImgHome.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Hàm xóa ảnh
 export async function API_DeleteImage(public_id: string, token: string) {
-  const res = await axios.delete(`http://localhost:8080/delete-image`, {
+  const res = await axios.delete(`https://bookingphoto.onrender.com/delete-image`, {
     headers: { Authorization: `Bearer ${token}` },
     data: { public_id },
   });
