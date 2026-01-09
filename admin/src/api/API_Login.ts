c719ddd70c13d75a8750b97ff851a510d50aeebf
Hoang Lam Bao Toan <160599423+hlbaot@users.noreply.github.com>
2026-01-09 09:18:39 +0700

    Update login API URL to production endpoint

--- admin/src/api/API_Login.ts
+++ admin/src/api/API_Login.ts
@@ -5,7 +5,7 @@ import { LoginRequest, LoginResponse } from "@/interfaces/login";
 // API request login
 export const API_Signin = async (values: LoginRequest): Promise<LoginResponse> => {
   const res = await axios.post<LoginResponse>(
-    "http://localhost:8080/auth/login",
+    "https://bookingphoto.onrender.com/auth/login",
     values
   );
   return res.data;
